fb3a19e7fba12988ddf1cb963c8aef7c940d8af2
falexramos <falexramos@gmail.com>
2021-09-17 21:15:17 -0500

    CREAR LA APLICACION
    crear la aplicacion angular

---
+++ DataBase/INSERT MATERIALES.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: select * from materiales
+LIMIT 0, 500
+
+-- Date: 2021-09-15 18:55
+*/
+INSERT INTO materiales (`construccion`,`cemento`,`grava`,`arena`,`madera`,`abobe`) VALUES ('CANCHA',20,20,20,20,20);
+INSERT INTO materiales (`construccion`,`cemento`,`grava`,`arena`,`madera`,`abobe`) VALUES ('CASA',100,50,90,20,100);
+INSERT INTO materiales (`construccion`,`cemento`,`grava`,`arena`,`madera`,`abobe`) VALUES ('EDIFICIO',200,100,180,40,200);
+INSERT INTO materiales (`construccion`,`cemento`,`grava`,`arena`,`madera`,`abobe`) VALUES ('GIMNASIO',50,25,45,10,50);
+INSERT INTO materiales (`construccion`,`cemento`,`grava`,`arena`,`madera`,`abobe`) VALUES ('LAGO',50,60,80,10,20);
